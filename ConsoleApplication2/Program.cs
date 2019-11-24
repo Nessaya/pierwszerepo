df3f2839bfa2f68d552590c1b41350fcfb0f5c3d
Nessaya <you@example.com>
2019-11-24 08:08:59 +0100

    Add infinity while to app

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -10,6 +10,12 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("1. dodaj liczby");
+            Console.WriteLine("2. odejmij liczby");
+            Console.WriteLine("3. pomnoz liczby");
+            Console.WriteLine("4. dziel liczby");
+            Console.WriteLine("5. wyjście");
+            int menuOption = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
